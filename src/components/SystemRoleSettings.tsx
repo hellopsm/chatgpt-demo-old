02555cfda31adaadb9222cc2a52ea1c983174725
hellopsm <psm2000163@163.com>
2023-04-01 13:31:30 +0800

    1
    1

---
+++ src/components/SystemRoleSettings.tsx
@@ -0,0 +1,80 @@
+import { Show } from 'solid-js'
+import type { Accessor, Setter } from 'solid-js'
+import IconEnv from './icons/Env'
+
+interface Props {
+  canEdit: Accessor<boolean>
+  systemRoleEditing: Accessor<boolean>
+  setSystemRoleEditing: Setter<boolean>
+  currentSystemRoleSettings: Accessor<string>
+  setCurrentSystemRoleSettings: Setter<string>
+}
+
+export default (props: Props) => {
+  let systemInputRef: HTMLTextAreaElement
+
+  const handleButtonClick = () => {
+    props.setCurrentSystemRoleSettings(systemInputRef.value)
+    props.setSystemRoleEditing(false)
+  }
+
+  return (
+    <div class="my-4">
+      <Show when={!props.systemRoleEditing()}>
+        <Show when={props.currentSystemRoleSettings()}>
+          <div class="text-slate">
+            <div class="flex items-center gap-1 op-60 text-slate">
+              <IconEnv />
+              <span>角色:</span>
+            </div>
+            <div class="mt-1">
+              { props.currentSystemRoleSettings() }
+            </div>
+          </div>
+        </Show>
+        <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
+          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="inline-flex items-center justify-center gap-1 text-sm text-slate bg-slate/20 px-2 py-1 rounded-md transition-colors cursor-pointer hover:bg-slate/50">
+            <IconEnv />
+            <span>点击设置扮演角色</span>
+          </span>
+        </Show>
+      </Show>
+      <Show when={props.systemRoleEditing() && props.canEdit()}>
+        <div>
+          <div class="flex items-center gap-1 op-60 text-slate">
+            <IconEnv />
+            <span>设置机器人扮演角色:</span>
+          </div>
+          <p class="my-2 leading-normal text-slate text-sm op-60">方便机器人更准确的回复您的指令.</p>
+          <div>
+            <textarea
+              ref={systemInputRef!}
+              placeholder="比如填写你是一名作家"
+              autocomplete="off"
+              autofocus
+              rows="3"
+              w-full
+              px-3 py-3 my-1
+              min-h-12
+              max-h-36
+              text-slate
+              rounded-sm
+              bg-slate
+              bg-op-15
+              focus:bg-op-20
+              focus:ring-0
+              focus:outline-none
+              placeholder:text-slate-400
+              placeholder:op-30
+              overflow-hidden
+              resize-none scroll-pa-8px
+            />
+          </div>
+          <button onClick={handleButtonClick} h-12 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 text-slate rounded-sm>
+            设置
+          </button>
+        </div>
+      </Show>
+    </div>
+  )
+}
